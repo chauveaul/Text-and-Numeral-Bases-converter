b5d036300729990c8291f80f359d92ca557f2e44
MeZeOfficial <lucaschauveau007@gmail.com>
2024-04-29 16:37:13 -0400

    Added hexadecimal conversion for all possible scenarios

--- script.js
+++ script.js
@@ -12,10 +12,18 @@ let convertTypeValue = "binary";
 
 //Listening when the user wants to convert
 btnConvertEl.addEventListener("click", function () {
-  if (originTypeEl.value === "text" && convertTypeEl.value === "binary")
-    textToBinary();
-  if (originTypeEl.value === "binary" && convertTypeEl.value === "text")
-    binaryToText();
+  if (originTypeEl.value === "text") {
+    if (convertTypeEl.value === "binary") textToBinary();
+    if (convertTypeEl.value === "hexadecimal") textToHex();
+  }
+  if (originTypeEl.value === "binary") {
+    if (convertTypeEl.value === "text") binaryToText();
+    if (convertTypeEl.value === "hexadecimal") binaryToHex();
+  }
+  if (originTypeEl.value === "hexadecimal") {
+    if (convertTypeEl.value === "text") hexToText();
+    if (convertTypeEl.value === "binary") hexToBinary();
+  }
 });
 
 //Simple implementation to convert when clicking enter
@@ -51,12 +59,11 @@ convertTypeEl.onchange = function () {
   inputAreaEl.value = "";
 };
 
-//To start off with the logic
-function textToBinary() {
-  const str = inputAreaEl.value.trim().split("");
+function textToBinary(source = inputAreaEl.value.trim()) {
+  source = source.split("");
   const binaryValues = [];
 
-  for (const char of str) {
+  for (const char of source) {
     //Storing the ascii value
     let charCode = char.charCodeAt(0);
     //Array to store the powers
@@ -86,12 +93,12 @@ function textToBinary() {
   }
 
   outputAreaEl.value = binaryValues.join(" ");
+  return binaryValues.join(" ");
 }
 
-function binaryToText() {
-  const source = inputAreaEl.value.split(" ");
+function binaryToText(source = inputAreaEl.value) {
   //TODO: add check for non-binary chars
-
+  source = source.split(" ");
   let result = "";
 
   for (const char of source) {
@@ -111,15 +118,140 @@ function binaryToText() {
   }
 
   outputAreaEl.value = result;
+  console.log(result);
+  return result;
+}
+
+function textToHex(source = inputAreaEl.value) {
+  source = source.split("");
+  const hexValues = [];
+
+  for (const char of source) {
+    let charCode = char.charCodeAt(0);
+
+    hexValues.push(decimalToByte(6, charCode));
+  }
+
+  outputAreaEl.value = hexValues.join(" ");
+  return hexValues.join(" ");
+}
+
+function binaryToHex() {
+  let source = inputAreaEl.value;
+  source = binaryToText(source);
+  source = textToHex(source);
+  outputAreaEl.value = source;
+}
+
+function hexToText(source = inputAreaEl.value) {
+  source = source.split(" ");
+  const chars = [];
+  for (const value of source) {
+    let decimalValue = 0;
+    decimalValue += Number(value[0]) * 16;
+    console.log(value[1]);
+    switch (value[1]) {
+      //Can easily refactor with difference between ascii code and reflect that on decimal value
+      case "A":
+        decimalValue += 10;
+        break;
+      case "B":
+        decimalValue += 11;
+        break;
+      case "C":
+        decimalValue += 12;
+        break;
+      case "D":
+        decimalValue += 13;
+        break;
+      case "E":
+        decimalValue += 14;
+        break;
+      case "F":
+        decimalValue += 15;
+        break;
+      default:
+        decimalValue += Number(value[1]);
+        break;
+    }
+    chars.push(String.fromCharCode(decimalValue));
+  }
+
+  outputAreaEl.value = chars.join("");
+  return chars.join("");
+}
+
+//Might just do hex --> text --> binary
+function hexToBinary() {
+  let source = inputAreaEl.value;
+  source = hexToText(source);
+  source = textToBinary(source);
+  outputAreaEl.value = source;
 }
 
 function decimalToByte(base, value) {
   let result = "";
-  while (value / base >= 1) {
+  //Binary transformation
+  if (base === 2) {
+    while (value / base >= 1) {
+      result = (value % base) + result;
+      value /= base;
+    }
     result = (value % base) + result;
-    value /= base;
   }
-  result = (value % base) + result;
+  //Hex transformation
+  else if (base === 6) {
+    for (let i = 15; i > 0; i--) {
+      if (i * 16 > value) continue;
+      switch (i) {
+        case 10:
+          result += "A";
+          break;
+        case 11:
+          result += "B";
+          break;
+        case 12:
+          result += "C";
+          break;
+        case 13:
+          result += "D";
+          break;
+        case 14:
+          result += "E";
+          break;
+        case 15:
+          result += "F";
+          break;
+        //If i is a single digit
+        default:
+          result += i;
+      }
+      value -= 16 * i;
+      break;
+    }
+    switch (value) {
+      case 10:
+        result += "A";
+        break;
+      case 11:
+        result += "B";
+        break;
+      case 12:
+        result += "C";
+        break;
+      case 13:
+        result += "D";
+        break;
+      case 14:
+        result += "E";
+        break;
+      case 15:
+        result += "F";
+        break;
+      default:
+        result += value;
+    }
+  }
   return result;
 }
 
